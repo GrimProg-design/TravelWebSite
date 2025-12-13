d9919b52408113d34896fcf06038e92ba1a96ae6
GrimProg <vavilovila01@gmail.com>
2025-12-13 21:25:27 +0600

    Добавил способы для работы с commonJS

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  optimizeDeps: {
+    include: [
+      'base64-js',
+      'buffer', 
+      'worldwindjs',
+    ],
+  },
+
+  resolve: {
+    alias: {
+      // Requires: npm install --save-dev buffer
+      // (Но часто optimizeDeps достаточно)
+      // 'buffer': 'buffer/' 
+    }
+  }
 })
